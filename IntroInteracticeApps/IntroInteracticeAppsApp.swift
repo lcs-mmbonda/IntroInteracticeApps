f68bc78af91817f9fbc1172cd7d9c1a02808f835
Myers <mmbonda@lcs.on.ca>
2024-01-15 11:38:22 -0500

    Initial Commit

---
+++ IntroInteracticeApps/IntroInteracticeAppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroInteracticeAppsApp.swift
+//  IntroInteracticeApps
+//
+//  Created by Myers Elliott Mbonda on 2024-01-15.
+//
+
+import SwiftUI
+
+@main
+struct IntroInteracticeAppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
